f387dd7ea74c09aa6f5f76ba9ee67284e83353dc
Bob Harris <rsharris@bx.psu.edu>
2020-11-16 14:51:25 -0500

    removed old alpha1/alpha2 sketch model

--- simulate_nucleotide_errors_v1.py
+++ simulate_nucleotide_errors_v1.py
@@ -10,13 +10,10 @@ from random       import Random,seed as random_seed, \
                          choice as random_choice, \
                          sample as random_sample
 from math         import sqrt,floor,ceil
-from heapq        import heapify,heappush,heappop
 from gzip         import open as gzip_open
-from mmh3         import hash128
 from kmer_mutation_formulas_v1 \
                   import p_affected,exp_n_affected,var_n_affected,estimate_r1_from_n_affected, \
-                         confidence_interval_r1_from_n_affected,in_confidence_interval_q_from_n_affected, \
-                         in_confidence_interval_jaccard_from_sketch_n_affected
+                         confidence_interval_r1_from_n_affected,in_confidence_interval_q_from_n_affected
 
 
 def usage(s=None):
@@ -24,8 +21,6 @@ def usage(s=None):
 usage: cat fasta | simulate_nucleotide_errors [options]
   --k=<N>                   (K=) kmer size
                             (default is 28)
-  --sketch=<N>              (S=) (cumulative) sketch size
-                            (default is "no sketch")
   --sequences=<N>           (T=) number of mutated sequences to generate
                             (default is 1)
   --poisson=<probability>   (P=) (required) inject random sequencing errors
@@ -51,10 +46,6 @@ usage: cat fasta | simulate_nucleotide_errors [options]
   --mutated=<filename>      file to write the mutated sequences to
   --mutateeonly             just write out the mutated sequences and quit
   --seed=<string>           set random seed
-  --hash=<int>              set seed for hash function (only used for sketches);
-                            it is highly recommended that users specify the
-                            hash seed
-                            (default is a 'randomly' chosen hash seed)
   --progress=<number>       periodically report how many sequence pairs we've
                             tested
 
@@ -71,22 +62,20 @@ def main():
 
 	# parse the command line
 
-	kmerSize           = 28
-	sketchSizes        = None
-	numSequences       = None
-	noiseKind          = None
-	pSubstitution      = None
-	sequenceType       = "linear"
-	confidence         = 0.99
-	ciUseInverse       = True
-	sortBy             = "nAffected"
-	statsFilename      = None
-	mutatedFilename    = None
-	mutateOnly         = False
-	prngSeed           = None
-	hashSeed           = None
-	reportProgress     = None
-	debug              = []
+	kmerSize        = 28
+	numSequences    = None
+	noiseKind       = None
+	pSubstitution   = None
+	sequenceType    = "linear"
+	confidence      = 0.99
+	ciUseInverse    = True
+	sortBy          = "nAffected"
+	statsFilename   = None
+	mutatedFilename = None
+	mutateOnly      = False
+	prngSeed        = None
+	reportProgress  = None
+	debug           = []
 
 	statsOfInterest = ["name",
 	                   "r1","k","L","confidence","trials","q",
@@ -99,9 +88,6 @@ def main():
 
 		if (arg.startswith("--kmer=")) or (arg.startswith("K=")):
 			kmerSize = int(argVal)
-		elif (arg.startswith("--sketch=")) or (arg.startswith("S=")):
-			if (sketchSizes == None): sketchSizes = []
-			sketchSizes += map(int,argVal.split(","))
 		elif (arg.startswith("--sequences=")) or (arg.startswith("T=")):
 			numSequences = int_with_unit(argVal)
 		elif (arg.startswith("--poisson=")) or (arg.startswith("--noise=")) or (arg.startswith("P=")):
@@ -128,8 +114,6 @@ def main():
 			mutateOnly = True
 		elif (arg.startswith("--seed=")):
 			prngSeed = argVal
-		elif (arg.startswith("--hash=")):
-			hashSeed = int(argVal)
 		elif (arg.startswith("--progress=")):
 			reportProgress = int_with_unit(argVal)
 		elif (arg == "--debug"):
@@ -151,33 +135,11 @@ def main():
 		# the presence of non-ACGT nucleotides isn't considered
 		usage("the bernoulli noise model is not currently supported")
 
-	if (sequenceType == "circular") and (sketchSizes != None):
-		# sketch_intersection() assumes linear sequences
-		usage("sketches are not currently supported for circular sequences")
-
 	if (sequenceType == "circular"):
 		# all the estimator code assumes linear sequences
 		usage("circular sequences are not currently supported")
 
-	if (sketchSizes != None):
-		sketchSizes = list(set(sketchSizes))   # (remove duplicates)
-		sketchSizes.sort()
-
-	if (sketchSizes != None):
-		for sketchSize in sketchSizes:
-			statsOfInterest += ["Mean[nIntersection(S=%d)].obs" % sketchSize,
-			                    "Mean[Jaccard(S=%d)].obs" % sketchSize,
-			                    "StDev[Jaccard(S=%d)].obs" % sketchSize,
-			                    "inCI(jest.nAff,S=%d).obs" % sketchSize]
-
 	# set up randomness
-	#
-	# note that we choose the hash seed randomly *before* seeding the PRNG, so
-	# that we (allegedly) get a randomly chosen hash; but users would be better
-	# off specifically choosing the hash seed
-
-	if (hashSeed == None):
-		hashSeed = int(0x100000000 * unit_random())
 
 	if (prngSeed != None):
 		random_seed(prngSeed.encode("utf-8"))
@@ -251,14 +213,6 @@ def main():
 	r1EstABObserved               = []
 	inConfR1EstABObserved         = []
 
-	if (sketchSizes != None):
-		nIntersectionObserved = {}
-		jaccardObserved       = {}
-		for sketchSize in sketchSizes:
-			nIntersectionObserved[sketchSize] = []
-			jaccardObserved[sketchSize]       = []
-			mutationModel.set_sketch_hash_seed(sketchSize,hashSeed)
-
 	for seqNum in range(numSequences):
 		if (reportProgress != None):
 			if (1+seqNum == 1) or ((1+seqNum) % reportProgress == 0):
@@ -295,14 +249,6 @@ def main():
 		                                                         nAffectedAB,useInverse=ciUseInverse)
 		inConfR1EstABObserved += [inConfR1EstAB]
 
-		# generate sketches and collect basic stats
-
-		if (sketchSizes != None):
-			for sketchSize in sketchSizes:
-				nIntersection = mutationModel.sketch_intersection(sketchSize)
-				nIntersectionObserved[sketchSize] += [nIntersection]
-				jaccardObserved[sketchSize]       += [float(nIntersection)/sketchSize]
-
 	# report per-trial results
 
 	if (sortBy == "nAffected"):
@@ -314,10 +260,6 @@ def main():
 		order = list(range(numSequences))
 
 	header = ["L","K","r","trial","nErr","nAff","r1est.nAff","|A|","|B|","|A^B|","|AuB|","nAff.A,B","L.A,B","r1est.A,B","inCI(r1est.A,B)"]
-	if (sketchSizes != None):
-		for sketchSize in sketchSizes:
-			header += ["nIntersection(s=%d)" % sketchSize]
-			header += ["j.est(nAff,s=%d)" % sketchSize]
 	print("#%s" % "\t".join(header))
 
 	for ix in range(numSequences):
@@ -337,10 +279,6 @@ def main():
 		        kmerSequenceLengthABObserved[order[ix]],  # L.A,B
 		        r1EstABObserved[order[ix]],               # r1est.A,B
 		        inConfR1EstABObserved[order[ix]])         # inCI(r1est.A,B)"]
-		if (sketchSizes != None):
-			for sketchSize in sketchSizes:
-				line += "\t%d"    % nIntersectionObserved[sketchSize][order[ix]]
-				line += "\t%0.9f" % jaccardObserved[sketchSize][order[ix]]
 		print(line)
 
 	if (mutatedF != None) and (mutatedF != stdout):
@@ -376,19 +314,6 @@ def main():
 	                     = sample_mean(kmerSequenceLengthABObserved)
 	r1EstABMean          = sample_mean(r1EstABObserved)
 
-	if (sketchSizes != None):
-		nIntersectionMean = {}
-		jaccardEstMean = {}
-		jaccardEstStDev = {}
-		inConfJaccardEstNAffected = {}
-		for sketchSize in sketchSizes:
-			nIntersectionMean        [sketchSize] = sample_mean(nIntersectionObserved[sketchSize])
-			jaccardEstMean           [sketchSize] = sample_mean(jaccardObserved[sketchSize])
-			jaccardEstStDev          [sketchSize] = sqrt(sample_variance(jaccardObserved[sketchSize]))
-			inConfJaccardEstNAffected[sketchSize] = in_confidence_interval_jaccard_from_sketch_n_affected \
-			                                          (kmerSequenceLength,kmerSize,sketchSize,pSubstitution,alpha,
-			                                           jaccardObserved[sketchSize],useInverse=ciUseInverse)
-
 	# report stats
 
 	statToText = {}
@@ -416,13 +341,6 @@ def main():
 	statToText["Mean[L.A,B].obs"]               = "%d"    % kmerSequenceLengthABMean
 	statToText["Mean[r1est.A,B].obs"]           = "%0.9f" % r1EstABMean
 	statToText["inCI(r1est.A,B).obs"]           = "%0.9f" % (float(sum(inConfR1EstABObserved)) / numSequences)
-
-	if (sketchSizes != None):
-		for sketchSize in sketchSizes:
-			statToText["Mean[nIntersection(S=%d)].obs" % sketchSize] = "%0.9f" % nIntersectionMean[sketchSize]
-			statToText["Mean[Jaccard(S=%d)].obs"       % sketchSize] = "%0.9f" % jaccardEstMean[sketchSize]
-			statToText["StDev[Jaccard(S=%d)].obs"      % sketchSize] = "%0.9f" % jaccardEstStDev[sketchSize]
-			statToText["inCI(jest.nAff,S=%d).obs"      % sketchSize] = "%0.9f" % (float(inConfJaccardEstNAffected[sketchSize]) / numSequences)
 
 	if (statsFilename != None):
 		if (statsFilename.endswith(".gz")) or (statsFilename.endswith(".gzip")):
@@ -455,14 +373,6 @@ class PoissonModel(object):
 		self.kmerSize            = kmerSize
 		self.pSubstitution       = pSubstitution
 		self.affectedKmerCounter = affectedKmerCounter
-		self.hashUniverse        = None
-		self.sketchHashSeeds     = {}
-
-	def set_hash_universe(self,hashUniverse):
-		self.hashUniverse = hashUniverse
-
-	def set_sketch_hash_seed(self,sketchSize,seed=0):
-		self.sketchHashSeeds[sketchSize] = seed & 0xFFFFFFFF  # (mmh3 seeds are limited to 32 bits)
 
 	def count(self,regenerate=False):
 		if (regenerate): self.generate()
@@ -489,58 +399,6 @@ class PoissonModel(object):
 			mutations = ntToMutations[nuc] if (nuc in ntToMutations) else "ACGT"
 			mutatedSeq[pos] = random_choice(mutations)
 		return "".join(mutatedSeq)
-
-	def sketch_intersection(self,sketchSize):
-		# note: this assumes linear sequences, not circular
-		# we consider the sketches to be multisets
-		# $$$ we should make that optional
-		assert (self.mutatedSeq != None)
-		if not (0 < sketchSize < 2*len(self.seq)): raise ValueError
-		nKmers = len(self.seq) - (self.kmerSize-1)
-		seed = self.sketchHashSeeds[sketchSize] if (sketchSize in self.sketchHashSeeds) else 0
-
-		# $$$ needs to exclude invalid kmers
-		sSketch = []    # heap for bottom sketch of seq kmers
-		mSketch = []    # heap for bottom sketch of mutated kmers
-		for pos in range(nKmers):
-			sH = hash128(self.seq       [pos:pos+self.kmerSize],seed)
-			mH = hash128(self.mutatedSeq[pos:pos+self.kmerSize],seed)
-			if (self.hashUniverse != None):
-				sH = sH % self.hashUniverse
-				mH = mH % self.hashUniverse
-			self.add_to_sketch(sSketch,sketchSize,sH)
-			self.add_to_sketch(mSketch,sketchSize,mH)
-
-		sSketch = list(map(lambda h:-h,sSketch))  # convert negated values
-		mSketch = list(map(lambda h:-h,mSketch))
-		sSketch.sort()                            # convert bottom sketches to sorted lists
-		mSketch.sort()
-
-		nIntersection = 0
-		sIx = mIx = 0
-		while (sIx < sketchSize) and (mIx < sketchSize):
-			while (sIx < sketchSize) and (sSketch[sIx] < mSketch[mIx]): sIx += 1
-			if (sIx == sketchSize): break
-			while (mIx < sketchSize) and (mSketch[mIx] < sSketch[sIx]): mIx += 1
-			if (mIx == sketchSize): break
-			if (sSketch[sIx] == mSketch[mIx]):
-				nIntersection += 1
-				sIx += 1
-				mIx += 1
-		return nIntersection
-
-	def add_to_sketch(self,sketch,sketchSize,val):
-		# We implement a sketch of size s by a max-heap, limiting its size to
-		# s. Thus we push elements as long as the size is less than s, and
-		# afterwards we only push elements that are less than the max.
-		#
-		# Note that the python heap implementation is a min-heap, so we negate
-		# the value that we receive and only push it if it's more than the min.
-		if (len(sketch) < sketchSize):
-			heappush(sketch,-val)
-		elif (-val > sketch[0]):
-			heappop(sketch)
-			heappush(sketch,-val)
 
 
 # BernoulliModel--
